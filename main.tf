88bc301b8dcda725bf9ff715adba6683609bfdcc
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-08-15 12:57:41 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,57 @@
+ resource "vcd_vapp" "vapp" {
+   name = var.vapp_name
+   description = var.vapp_description
+   org = var.org_name
+   vdc = var.vdc_name
+   power_on = true
+     lease {
+    runtime_lease_in_sec = 0 # extends the runtime lease to 30 days
+    storage_lease_in_sec = 0  # extends the storage lease to 7 days
+  }
+ }
+
+ resource "vcd_vapp_org_network" "vappOrgNet" {
+  org = var.org_name
+  vdc = var.vdc_name
+  vapp_name = var.vapp_name
+
+  # Comment below line to create an isolated vApp network
+  org_network_name = var.org_network_name
+}
+
+resource "vcd_vapp_vm" "web1" {
+  vapp_name     = var.vapp_name
+  name          = var.vm_name
+  catalog_name  = var.catalog_name
+  template_name = var.template_name
+  memory        = var.vm_memory
+  cpus          = var.vm_cpu
+  cpu_cores     = var.vm_cpu_core
+  cpu_hot_add_enabled=true
+  memory_hot_add_enabled=true
+  power_on=false
+
+    override_template_disk {
+    bus_type        = "paravirtual"
+    size_in_mb      = var.vm_disk_size
+    bus_number      = 0
+    unit_number     = 0
+    iops            = 0
+  }
+
+
+  network {
+    type               = "org"
+    name               = var.org_network_name
+    ip_allocation_mode = "MANUAL"
+    ip                 = var.vm_ip
+    adapter_type       = "VMXNET3"
+    is_primary         = true
+  }
+
+   customization{
+      enabled=false
+    }
+
+
+}
